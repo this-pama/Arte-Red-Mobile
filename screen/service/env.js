9cf4fb0841b887dfe9aada5275b752b5a9a5bdc4
Dapo Aderemi <adedapopaul@yahoo.com>
2019-10-20 15:36:12 +0100

    change url to remote

--- screen/service/env.js
+++ screen/service/env.js
@@ -3,7 +3,7 @@ export const cloudinaryUrl = "https://api.cloudinary.com/v1_1/artered/image/uplo
 
 const heroku = "https://artered.herokuapp.com/v1/"
 const localUrl = "http://192.168.8.100:3000/v1/"
-export const apiUrl = localUrl
+export const apiUrl = heroku
 
 
 export const ravePublicKey = "FLWPUBK_TEST-eef2977c76dfbc31ea5015d213983f2e-X"

590bdcb4ad7fcb544b49a2e04680e101029681c5
Dapo Aderemi <adedapopaul@yahoo.com>
2020-01-03 16:07:11 +0100

    implement fund wallet screen

--- screen/fundWallet.js
+++ screen/fundWallet.js
@@ -4,6 +4,7 @@ import { Container, Header, Left, Button,Icon, Body,Title,Right,
   Input, Form, Spinner,  } from 'native-base';
 import PropTypes from "prop-types"
 import { View, KeyboardAvoidingView } from "react-native"
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import {BackHandler} from "react-native"
 import { loginAction } from "../redux/loginAction"
 import { getUserIdAction } from "../redux/getUserId"
@@ -14,7 +15,7 @@ import {apiUrl} from "./service/env"
 import {connect} from 'react-redux'
 import Modal, { ModalContent, ModalFooter, ModalButton, SlideAnimation, ModalTitle, } from 'react-native-modals';
 import { CreditCardInput, LiteCreditCardInput } from "react-native-credit-card-input";
- 
+import { WebView } from 'react-native-webview';
 
 
 class FundWalletScreen extends Component{
@@ -28,6 +29,7 @@ class FundWalletScreen extends Component{
         currency: '',
         showCard : false,
         showAmount: true,
+        showAddress: false,
         cardno: '',
         cvv: '',
         expiry: '',
@@ -35,6 +37,21 @@ class FundWalletScreen extends Component{
         nextCard: false,
         wallet: {},
         modalVisible: false,
+        showOtp: false,
+        showPin: false,
+        showWebView: false,
+        url: '',
+        authMode: null,
+        pin: '',
+        billingaddress: '',
+        billingcity: '',
+        billingcountry: '',
+        billingstate: '',
+        billingzip: '',
+        otp: '',
+        otpReference: '',
+        fetched: true,
+
     }
 }
 
@@ -110,6 +127,176 @@ class FundWalletScreen extends Component{
     }
   };
 
+  handlePin = pin => {
+    if (pin ) {
+      this.setState(
+        {
+          pin,
+          disable: false,
+          errMessage: ''
+        },
+        this.validateFormAmount
+      );
+    } else {
+      this.setState({
+        pin: '',
+        errMessage: 'Input PIN'
+      });
+    }
+  };
+
+  handleOtp = otp => {
+    if (otp ) {
+      this.setState(
+        {
+          otp,
+          disable: false,
+          errMessage: ''
+        },
+        this.validateFormAmount
+      );
+    } else {
+      this.setState({
+        otp: '',
+        errMessage: 'Input OTP'
+      });
+    }
+  };
+
+  processOtp= async ()=>{
+    this.setState({ fetched : false })
+    var url = apiUrl + "wallet/payment/validate";
+    var result = await fetch(url, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        reference: this.state.otpReference, 
+        otp: this.state.otp
+      })
+    });
+    var response = await result;
+    if(response.status !== 200 ){
+      this.setState({
+        message: 'Transaction Validation failed',
+        modalVisible: true,
+        fetched : true
+      })
+    }
+    else{
+        var res = await response.json()
+        // console.warn('response message', res.message)
+        if(res.success ){
+          this.setState({
+            message: 'Transaction successful',
+            modalVisible: true,
+            showAmount: true,
+            showOtp: false,
+            fetched : true,
+            errMessage: '',
+          })
+        }
+        else{
+          this.setState({
+            message: 'Transaction Validation failed',
+            modalVisible: true,
+            fetched : true,
+          })
+        }
+    }
+    
+  }
+
+  handleBillingZip = billingzip => {
+    if (billingzip) {
+      this.setState(
+        {
+          billingzip,
+          disable: false,
+          errMessage: ''
+        },
+        this.validateFormAmount
+      );
+    } else {
+      this.setState({
+        billingzip: '',
+        errMessage: 'Input Billing Zip'
+      });
+    }
+  };
+
+  handleBillingState = billingstate => {
+    if (billingstate) {
+      this.setState(
+        {
+          billingstate,
+          disable: false,
+          errMessage: ''
+        },
+        this.validateFormAmount
+      );
+    } else {
+      this.setState({
+        billingstate: '',
+        errMessage: 'Input Billing State'
+      });
+    }
+  };
+
+  handleBillingCountry = billingcountry => {
+    if (billingcountry) {
+      this.setState(
+        {
+          billingcountry,
+          disable: false,
+          errMessage: ''
+        },
+        this.validateFormAmount
+      );
+    } else {
+      this.setState({
+        billingcountry: '',
+        errMessage: 'Input Billing Country'
+      });
+    }
+  };
+
+  handleBillingCity = billingcity => {
+    if (billingcity) {
+      this.setState(
+        {
+          billingcity,
+          disable: false,
+          errMessage: ''
+        },
+        this.validateFormAmount
+      );
+    } else {
+      this.setState({
+        billingcity: '',
+        errMessage: 'Input Billing City'
+      });
+    }
+  };
+
+  handleBillingAddress = billingaddress => {
+    if (billingaddress) {
+      this.setState(
+        {
+          billingaddress,
+          disable: false,
+          errMessage: ''
+        },
+        this.validateFormAmount
+      );
+    } else {
+      this.setState({
+        billingaddress: '',
+        errMessage: 'Input Billing Address'
+      });
+    }
+  };
+
+
   validateFormAmount = () => {
     if( +this.state.amount > 0 && this.state.currency.length > 0 && 
       this.state.currency.length < 4){
@@ -128,8 +315,9 @@ class FundWalletScreen extends Component{
   }
 
   _onChange = form => {
+    let cardno = form.values.number.replace(/\s/g,'')
       this.setState({
-          cardno : form.values.number,
+          cardno,
           cvv: form.values.cvc,
           expiry: form.values.expiry,
           name: form.values.name
@@ -143,7 +331,352 @@ class FundWalletScreen extends Component{
       }
 }
 
+initiatePayment= async () =>{
+    this.setState({ processWithdrawal : false })
+    var url = apiUrl + "wallet/payment/card/" + this.props.userId;
+    const expiry = this.state.expiry.replace('/', '')
+    let month = expiry.substring(0, 2);
+    let year = expiry.substring(2, this.state.expiry.length - 1);
+    let data
+
+    switch (this.state.authMode) {
+      case "PIN":
+        data = {
+          amount : this.state.amount,
+          currency: this.state.currency,
+          cardno : this.state.cardno,
+          cvv: this.state.cvv,
+          expiryMonth : month,
+          expiryYear: year,
+          pin: this.state.pin,
+          auth: "PIN"
+        }
+        break;
+
+      case 'AVS_VBVSECURECODE':
+          data = {
+            amount : this.state.amount,
+            currency: this.state.currency,
+            cardno : this.state.cardno,
+            cvv: this.state.cvv,
+            expiryMonth : month,
+            expiryYear: year,
+            pin: this.state.pin,
+            auth: "AVS_VBVSECURECODE",
+            billingzip: this.state.billingzip,
+            billingcity: this.state.billingcity,
+            billingaddress: this.state.billingaddress,
+            billingstate: this.state.billingstate,
+            billingcountry: this.state.billingcountry,
+          }
+        
+        break;
+
+      case "NOAUTH_INTERNATIONAL":
+         data = {
+            amount : this.state.amount,
+            currency: this.state.currency,
+            cardno : this.state.cardno,
+            cvv: this.state.cvv,
+            expiryMonth : month,
+            expiryYear: year,
+            pin: this.state.pin,
+            auth: "NOAUTH_INTERNATIONAL",
+            billingzip: this.state.billingzip,
+            billingcity: this.state.billingcity,
+            billingaddress: this.state.billingaddress,
+            billingstate: this.state.billingstate,
+            billingcountry: this.state.billingcountry,
+          }
+        break;
+
+      default: 
+      data = {
+        amount : this.state.amount,
+        currency: this.state.currency,
+        cardno : this.state.cardno,
+        cvv: this.state.cvv,
+        expiryMonth : month,
+        expiryYear: year,
+      }
+    }
+
+    this.setState({ fetched : false })
+    // make card request to server
+    var result = await fetch(url, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(data)
+    });
+    var response = await result;
+    if(response.status !== 200 ){
+      this.setState({
+        message: 'Transaction Error occured',
+        modalVisible: true,
+        fetched : true
+      })
+    }
+    else{
+      var res = await response.json();
+      console.warn(res)
+      if(res.success) {
+          if(res.data.data.suggested_auth && res.data.data.suggested_auth == 'PIN'){
+              this.setState({
+                  showOtp: false,
+                  showPin: true,
+                  showCard: false,
+                  showAmount: false,
+                  errMessage: "Input Card PIN",
+                  nextCard: false,
+                  authMode: res.data.data.suggested_auth,
+                  fetched : true
+              })
+          }
+
+          else if(res.instruction && res.instruction.includes('OTP') && res.url  && res.url.length > 0 && res.reference && res.reference.length > 0 ){
+            this.setState({
+                showOtp: true,
+                showPin: false,
+                showCard: false,
+                showAmount: false,
+                errMessage: res.instruction,
+                nextCard: false,
+                otpReference: res.reference,
+                fetched : true
+            })
+          }
+          else if(res.data.data.suggested_auth && (res.data.data.suggested_auth == "NOAUTH_INTERNATIONAL" || res.data.data.suggested_auth == "AVS_VBVSECURECODE" )){
+            this.setState({
+                showOtp: false,
+                showPin: false,
+                showCard: false,
+                showAmount: false,
+                showAddress: true,
+                errMessage: "",
+                nextCard: false,
+                authMode: res.data.data.suggested_auth,
+                fetched : true
+            })
+        }
+          // else if( res.message.includes('Requires validation') && res.url && res.url.length > 0){
+          //   // console.warn(res.instruction)
+          //     this.setState({
+          //         errMessage: res.instruction,
+          //         showWebView : true,
+          //         showAmount: false,
+          //         showCard: false,
+          //         nextCard: false,
+          //         url: res.url,
+          //     })
+
+          //     return 
+          // }
+          else{
+              this.setState({
+                  message: 'Transaction failed',
+                  modalVisible: true,
+                  errMessage: '',
+                  showOtp: false,
+                  showAmount: false,
+                  showCard: false,
+                  nextCard: false,
+                  showWebView: false,
+                  showOtp: false,
+                  showPin: false,
+                  showAddress: false,
+              })
+          }
+ 
+      } 
+      else  {
+        this.setState({
+          message: 'There seems to be an error',
+          modalVisible: true,
+          showOtp: false,
+            showAmount: false,
+            showCard: false,
+            showWebView: false,
+            nextCard: false,
+            errMessage: res.instruction
+        })
+      }
+    }
+  }
+
+  webview = null;
+
+  handleWebViewNavigationStateChange = newNavState => {
+    // newNavState looks something like this:
+    // {
+    //   url?: string;
+    //   title?: string;
+    //   loading?: boolean;
+    //   canGoBack?: boolean;
+    //   canGoForward?: boolean;
+    // }
+    const { url } = newNavState;
+    if (!url) return;
+
+    // one way to handle a successful form submit is via query strings
+    if (url.includes('message=Approved.+Successful&receiptno')) {
+      this.webview.stopLoading();
+      // maybe close this view?
+      this.setState({
+        showWebView: false,
+        showAmount: true,
+        modalVisible: true,
+        message: "Transaction Successful",
+        errMessage: '',
+      })
+    }
+    // one way to handle errors is via query string
+    if (url.includes('?response=')) {
+      this.webview.stopLoading();
+      this.setState({
+        showWebView: false,
+        showAmount: true,
+        modalVisible: true,
+        message: "Transaction failed",
+        errMessage: '',
+      })
+    }
+
+    // one way to handle errors is via query string
+    if (url.includes('?errors=true')) {
+      this.webview.stopLoading();
+      this.setState({
+        showWebView: false,
+        showAmount: true,
+        modalVisible: true,
+        message: "Error occurred",
+        errMessage: '',
+      })
+    }
+
+    // else{
+    //     this.webview.stopLoading();
+    //     alert('Nothing happened')
+    // }
+
+  };
+
     render(){
+      const showOtp = (
+            <View>
+                <ListItem>
+                <Body>
+                  <Item stackedLabel>
+                      <Label>OTP</Label>
+                      <Input onChangeText= {this.handleOtp  } value={this.state.otp}  keyboardType='numeric' />
+                  </Item>
+                </Body>
+            </ListItem>
+            <Button  block danger 
+                disabled={this.state.disable}
+                onPress={ this.processOtp }
+            >
+                { this.state.fetched ? <Text> Next </Text> : <Spinner color='white' /> }
+            </Button>
+          </View>
+      )
+
+      const showPin = (
+        <View>
+            <ListItem>
+            <Body>
+              <Item stackedLabel>
+                  <Label>PIN</Label>
+                  <Input onChangeText= {this.handlePin  } value={this.state.pin}  keyboardType='numeric' />
+              </Item>
+            </Body>
+        </ListItem>
+        <Button  block danger 
+            disabled={this.state.disable}
+            onPress={ this.initiatePayment }
+        >
+            { this.state.fetched ? <Text> Next </Text> : <Spinner color='white' /> }
+        </Button>
+      </View>
+  )
+
+  showAddress= (
+    <View>
+      <Item stackedLabel>
+          <Label>Billing Address</Label>
+          <Input onChangeText= { this.handleBillingAddress} value={this.state.billingaddress }   />
+      </Item>
+      <Item stackedLabel>
+          <Label>Billing City</Label>
+          <Input onChangeText= { this.handleBillingCity} value={this.state.billingcity}   />
+      </Item>
+      <Item stackedLabel>
+          <Label>Billing Zip</Label>
+          <Input onChangeText= { this.handleBillingZip } value={this.state.billingzip }   />
+      </Item>
+      <Item stackedLabel>
+          <Label>Billing State</Label>
+          <Input onChangeText= { this.handleBillingState} value={this.state.billingstate }   />
+      </Item>
+      <Item stackedLabel>
+          <Label>Billing Country</Label>
+          <Input onChangeText= { this.handleBillingCountry} value={this.state.billingcountry}   />
+      </Item>
+      <Button  block danger 
+            disabled={this.state.disable}
+            onPress={ this.initiatePayment }
+        >
+            { this.state.fetched ? <Text> Next </Text> : <Spinner color='white' /> }
+        </Button>
+    </View>
+  )
+
+      const run = `
+      document.body.style.backgroundColor = 'red';
+      true;
+    `;
+
+      const showWebView = (
+        <WebView
+          ref={ref => (this.webview = ref)}
+          source={{ uri: this.state.url }}
+          onNavigationStateChange={this.handleWebViewNavigationStateChange}
+          style={{ marginTop: 50 }}
+          scalesPageToFit= { false }
+          startInLoadingState={true}
+          renderLoading={() => <Spinner color='red' /> }
+          // injectedJavaScript={run}
+          onMessage={event => {
+            console.warn('event.nativeEvent.data', event.nativeEvent.data);
+            if(event.nativeEvent.data.include('"response":"{}"')){
+              this.setState({
+                showAmount: true,
+                showWebView: false,
+                modalVisible: true,
+                message: "Transaction failed"
+              })
+            }
+            // else if(event.nativeEvent.data.include()){
+            //   this.setState({
+            //     showAmount: true,
+            //     showWebView: false,
+            //     modalVisible: true,
+            //     message: "Transaction failed"
+            //   })
+            // }
+            else{
+              this.setState({
+                showAmount: true,
+                showWebView: false,
+                modalVisible: true,
+                message: "Transaction failed"
+              })
+            }
+            
+          }}
+        />
+      )
+
+      if(!this.state.showWebView){
         return(
           <Container>
         <Header  style={{ backgroundColor: "#990000", paddingTop: 50, paddingBottom: 40 }}>
@@ -162,20 +695,31 @@ class FundWalletScreen extends Component{
           </Right>
         </Header>
         <Content style={{ padding: 20,}}>
+        <KeyboardAwareScrollView
+          extraScrollHeight={100}
+          enableOnAndroid={true} 
+          keyboardShouldPersistTaps='handled'
+        >
           <Body><Text style={{ color : 'red'}}>{ this.state.errMessage }</Text></Body>
-          { this.state.showCard ? (<CreditCardInput onChange={this._onChange} requiresName= { true } requiresCVC= { true} />) : null }
+          { this.state.showCard ? (<CreditCardInput onChange={this._onChange} requiresName= { true } requiresCVC= { true} allowScroll= { true } />) : null }
 
           { this.state.nextCard ? (
               <View style={{ paddingTop: 20 }}>
                   <Button  block danger 
                     //   disabled={this.state.disable}
-                    onPress={ this.nextAmount }
+                    onPress={ this.initiatePayment }
                 >
-                    <Text> Next </Text>
+                    { this.state.fetched ? <Text> Next </Text> : <Spinner color='white' /> }
                 </Button>
               </View>
           ) : null }
 
+          { this.state.showOtp ? showOtp : null } 
+
+          { this.state.showPin ? showPin : null }
+
+          { this.state.showAddress ? showAddress : null }
+          
           { this.state.showAmount ? (
             <View>
                 <ListItem>
@@ -199,6 +743,7 @@ class FundWalletScreen extends Component{
             </Button>
           </View>
           ) : null }
+          </KeyboardAwareScrollView>
         </Content>
         <Footer >
           <FooterTab style={{ color: "#ffcccc", backgroundColor: "#990000"}}>
@@ -215,7 +760,7 @@ class FundWalletScreen extends Component{
               <Text>Wallet</Text>
             </Button>
             <Button vertical 
-              onPress={()=> this.props.navigation.navigate("Bank")} >
+              onPress={()=> this.props.navigation.navigate("BankDetails")} >
               <Icon name="home" />
               <Text>Bank</Text>
             </Button>
@@ -253,9 +798,12 @@ class FundWalletScreen extends Component{
       </Modal>
       </Container>
         )
+      }
+      else{
+        return showWebView
+      }
     }
 }
-
 
 const mapStateToProps = state => ({
   jwt: state.login.jwt,
